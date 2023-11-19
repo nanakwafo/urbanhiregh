ed22f95c679f0d5ea8ffdbfa28c1603e9f32d042
nanakwafo <nanamensah1140@gmail.com>
2023-11-19 19:57:16 +0000

    About us content

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -50,19 +50,18 @@
 <div class="content content-fixed content-auth">
     <div class="container">
         <div class="media align-items-stretch justify-content-center ht-100p">
-            <div class="sign-wrapper mg-lg-r-50 mg-xl-r-60">
+
                 <div class="pd-t-20 wd-100p">
                     <h4 class="tx-color-01 mg-b-5">Privacy Policy </h4>
-                    <p class="tx-color-03 tx-16 mg-b-40">It's free to signup and only takes a minute.</p>
+                    <p class="tx-color-03 tx-16 mg-b-40">Urbanhire is the UK's trusted online trade directory, for which we must thank both the public for their strong support of our service, and our great traders who prove that there is an alternative to the 'cowboys'.</p>
+                    <p class="tx-color-03 tx-16 mg-b-40">You know how frustrating it is to have to use a trader directly out of the paper or a directory without getting recommendations first. But if an emergency occurred, say in the middle of the night or at the weekend, then you simply had no choice in the matter.</p>
+                    <p class="tx-color-03 tx-16 mg-b-40"> If work isn't carried out to your satisfaction, the whole experience can leave a bitter taste in your mouth, not to mention a hole in your pocket.</p>
+                    <p class="tx-color-03 tx-16 mg-b-40">Wouldn't it be great if there was a website that could put you in touch with local, reliable tradesmen whose work had been checked out - a website that was as good as a personal recommendation? That's what we thought too. So, we made it our mission to create the Web's most trusted online directory when looking for tradesmen - not just for emergency 'out of hours' jobs but for any job, anytime. Welcome to Urbanhire.</p>
+                    <p class="tx-color-03 tx-16 mg-b-40">Our 'Leave a Review' facility allows you, the consumer, to rate the work of the tradesperson or business you've used. This, together with the fact that we check that all our tradesmen have a minimum of two year's experience in their field and are members of the relevant trade association, makes us the safest bet on the Net when searching for a tradesperson.</p>
 
                 </div>
-            </div><!-- sign-wrapper -->
-            <div class="media-body pd-y-30 pd-lg-x-50 pd-xl-x-60 align-items-center d-none d-lg-flex pos-relative">
-                <div class="mx-lg-wd-500 mx-xl-wd-550">
-                    <img src="{{asset('assets/img/close-up-hand-repairman-professional-builder-working-indoors-repairing.png')}}" class="img-fluid" alt="">
-                </div>
 
-            </div><!-- media-body -->
+
         </div><!-- media -->
     </div><!-- container -->
 </div><!-- content -->
