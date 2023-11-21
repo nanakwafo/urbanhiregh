80ebe780206571b174642e043cddb6c2094c814a
nanakwafo <nanamensah1140@gmail.com>
2023-11-21 15:19:25 +0000

    Added faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -50,19 +50,67 @@
 <div class="content content-fixed content-auth">
     <div class="container">
         <div class="media align-items-stretch justify-content-center ht-100p">
-            <div class="sign-wrapper mg-lg-r-50 mg-xl-r-60">
-                <div class="pd-t-20 wd-100p">
-                    <h4 class="tx-color-01 mg-b-5"> FAQ's </h4>
-                    <p class="tx-color-03 tx-16 mg-b-40">It's free to signup and only takes a minute.</p>
 
-                </div>
-            </div><!-- sign-wrapper -->
-            <div class="media-body pd-y-30 pd-lg-x-50 pd-xl-x-60 align-items-center d-none d-lg-flex pos-relative">
-                <div class="mx-lg-wd-500 mx-xl-wd-550">
-                    <img src="{{asset('assets/img/close-up-hand-repairman-professional-builder-working-indoors-repairing.png')}}" class="img-fluid" alt="">
+            <div class="pd-t-20 wd-100p accordion">
+
+                <p class="tx-color-03 tx-16 mg-b-40">
+                    New to Urbanhire? Read our Frequently Asked Questions, with lists tailored to both consumers and tradespeople.
+                </p>
+
+
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>Why do I need to be registered to leave a review?</h4>
+                <p>We only allow registered consumers to leave reviews because we need the contact details of a consumer in the case of a bad review.
+
+                    To ensure reviews of our traders are accurate, we contact a consumer who leaves a less positive review in order to validate their story with the trader.
+                </p>
                 </div>
 
-            </div><!-- media-body -->
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>How do I know I can rely on this trader to do the job properly?</h4>
+                <p> At urbanhiregh.com, we do all the hard work for you! We have hundreds of local tradesmen in your area who have been vetted by our team to check on all the questions you'd like to ask yourself.
+                    If the feedback isn't good enough when they request to join up to urbanhiregh.com, they won't be on our website. Simple!
+                </p>
+                </div>
+
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>What do you do to make sure tradesmen are qualified to do work such as gas or electrics?</h4>
+                <p> There are certain trades where experience isn't always enough! If you have gas or engineering works to be done, you need to know that whoever you hire is the best person for the job. We require copies of all current certificates when our members join, so we know they have the skills you need to do a professional job.
+                    For electricians, we require proof of registration with a governing body, and proof of Part P and 17th Addition Compliance; this ensures that our traders have their specialist training up to date, as bodies such as NICEIC have the sole purpose of protecting consumers from unsafe and unsound electrical work. The same goes for the Gas Safe Register; any trader coming into your home to carry out any gas works will have current Gas Safe Registration if you find them through Trustatrader.com.
+                    It's always a good idea to check with the trader when they come round, and ask to see their current membership cards for your total peace of mind.
+                </p>
+                </div>
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>How does urbanhiregh.com work?</h4>
+                <p> Unlike some websites advertising reliable and trustworthy tradespeople, we are only here to make sure that you, the consumer, have the best choice of professionals to do the job. Our stringent vetting and constant monitoring of our traders provides you with the assurance that any trader you choose will be capable of completing your job to a high standard. You are, however, welcome to call as many of the companies as you like until you find the perfect one for your needs. </p>
+                </div>
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>How do I choose the right trader for the job?</h4>
+                <p>That really is up to you! We've done all the hard work by ensuring that all our traders are the best of the best, but you can make choices as a result of the following:
+
+                    The reviews which have been left on the website
+                    The trader's profile
+                    The trader's qualifications and memberships (these are listed with logos and in the qualifications box)
+                    Photographs of previous work</p>
+                </div>
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>Is there a charge to the consumer to use urbanhiregh.com?</h4>
+                <p>urbanhire is a free online directory of tradespeople you can trust because their place on the website depends upon their quality of service. Consumers can leave feedback for our traders based on the work that has been done for them, which continues to improve the reliability of the service.</p>
+                </div>
+
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                <h4>I'm a consumer. How do I get the best out of urbanhiregh.com?</h4>
+                <p>As a recommendations website, you can rest assured of the rigorous and stringent checks carried out on all tradespeople. We do recommend however that you contact at least two or three traders to get an idea of costs and how you get on with the tradesperson. There's no right or wrong, and we have no regulations on the fees our traders charge, so make sure you get enough details to ensure you use the company that's right for you.</p>
+                </div>
+
+
+
+            </div>
+
+
         </div><!-- media -->
     </div><!-- container -->
 </div><!-- content -->
@@ -75,26 +123,27 @@
 <script src="../../lib/feather-icons/feather.min.js"></script>
 <script src="../../lib/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <script src="../../lib/select2/js/select2.min.js"></script>
-
+<script src="../../lib/jqueryui/jquery-ui.min.js"></script>
+<script src="../../lib/prismjs/prism.js"></script>
 <script src="../../assets/js/dashforge.js"></script>
 
 <!-- append theme customizer -->
 <script src="../../lib/js-cookie/js.cookie.js"></script>
 <script src="../../assets/js/dashforge.settings.js"></script>
 <script>
-    $(function(){
+    $(function () {
         'use script'
 
-        window.darkMode = function(){
+        window.darkMode = function () {
             $('.btn-white').addClass('btn-dark').removeClass('btn-white');
         }
 
-        window.lightMode = function() {
+        window.lightMode = function () {
             $('.btn-dark').addClass('btn-white').removeClass('btn-dark');
         }
 
         var hasMode = Cookies.get('df-mode');
-        if(hasMode === 'dark') {
+        if (hasMode === 'dark') {
             darkMode();
         } else {
             lightMode();
@@ -104,6 +153,9 @@
             placeholder: 'Choose one',
             searchInputPlaceholder: 'Search options'
         })
+        $('#accordion').accordion({
+            heightStyle: 'content'
+        });
     })
 </script>
 </body>
