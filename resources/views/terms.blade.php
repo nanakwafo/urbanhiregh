520b600031e7c0fbf56dea643fcbf092c12f6085
nanakwafo <nanamensah1140@gmail.com>
2023-11-21 14:38:43 +0000

    terms and condition page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -50,19 +50,190 @@
 <div class="content content-fixed content-auth">
     <div class="container">
         <div class="media align-items-stretch justify-content-center ht-100p">
-            <div class="sign-wrapper mg-lg-r-50 mg-xl-r-60">
-                <div class="pd-t-20 wd-100p">
-                    <h4 class="tx-color-01 mg-b-5">Terms & Condition </h4>
-                    <p class="tx-color-03 tx-16 mg-b-40">It's free to signup and only takes a minute.</p>
 
-                </div>
-            </div><!-- sign-wrapper -->
-            <div class="media-body pd-y-30 pd-lg-x-50 pd-xl-x-60 align-items-center d-none d-lg-flex pos-relative">
-                <div class="mx-lg-wd-500 mx-xl-wd-550">
-                    <img src="{{asset('assets/img/close-up-hand-repairman-professional-builder-working-indoors-repairing.png')}}" class="img-fluid" alt="">
+            <div class="pd-t-20 wd-100p">
+
+                <p class="tx-color-03 tx-16 mg-b-40">These Website Terms and Use (“Terms”) tell you the rules for accessing and using our website www.urbanhire.com (our "Website").
+
+                    These Website Terms of Use were last updated on 28th June 2021</p>
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Who we are and how to contact us</h4>
+                    <p>Trustatrader and our Website is operated and managed by Trustatrader.com Ltd. We are a company incorporated in Ghana and Wales (company number 05489753). Our registered office address is at 5 Meadway Court, Rutherford Close, Stevenage, Hertfordshire, Ghana, SG1 2EF) (“Trustatrader”, “we”, “us”, “our”).
+
+                        To contact us, please email admin@urbanhire.com or write to us at 5 Meadway Court, Rutherford Close, Stevenage SG1 2EF.</p>
                 </div>
 
-            </div><!-- media-body -->
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Our Website and acceptance of these Terms</h4>
+                    <p>We provide an online directory service through which visitors and users of our Website (“Visitors”) can access information about local tradespersons who are registered members of our Website (“Traders”).
+
+                        Please read these Terms carefully before using our Website. For Visitors, we particularly draw your attention to the following paragraphs in these Terms:
+
+                        Do not rely on information on our Website
+                        Disclaimer
+                        Our responsibility for loss or damage suffered by you
+                        By using our Website, you confirm that you accept these Terms and that you agree to comply with them. If you do not agree to these Terms, you must not use our Website.
+
+                        We recommend that you print a copy of these Terms for future reference.</p>
+                </div>
+
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>There are other terms that may apply to you</h4>
+                    <p>These Terms refer to the following additional terms, which also apply to your use of our Website and services:
+
+                        Our Privacy Policy. See further information under the heading How We May Use Your Personal Information.
+                        Our Cookie Policy, which sets out information about the cookies we use on our Website.
+                        Our Trader Terms and Conditions, which apply to Traders only.
+                    </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>We may make changes to these Terms</h4>
+                    <p>We may need to amend these Terms from time to time. Every time you wish to use our Website, please check these Terms to ensure you understand the terms that apply at that time.
+
+                        These Terms were last updated on the date stated at the beginning of these Terms.
+                    </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>We may make changes to our Website</h4>
+                    <p>We may update and change our Website from time to time to reflect changes to our services, our users' needs and our business priorities.
+                    </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>We may suspend or withdraw our Website</h4>
+                    <p>Our Website is made available free of charge.
+
+                        We do not guarantee that our Website, or any content on it, will always be available or be uninterrupted. We may suspend or withdraw or restrict the availability of all or any part of our Website for business and operational reasons.
+
+                        You are responsible for ensuring that all persons who access our Website through your internet connection are aware of these Terms and other applicable terms and conditions, and that they comply with them.
+                    </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>We may transfer these Terms to someone else
+                    </h4>
+                    <p>We may transfer our rights and obligations under these Terms to another organisation. We will always tell you in writing if this happens and we will ensure that the transfer will not affect your rights under these Terms.   </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>How you may use material on our Website
+                    </h4>
+                    <p>We are the owner or the licensee of all intellectual property rights in our Website, and in the material published on it. Those works are protected by copyright laws and treaties around the world. All such rights are reserved. </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Do not rely on information on our Website
+                    </h4>
+                    <p>Although we make reasonable efforts to update the information on our Website, we make no representations, warranties or guarantees, whether express or implied, that the content on our Website or any linked website, is accurate, complete, error free or up to date.
+
+                        Without limiting the generality of the foregoing, we are not responsible for any content, communications, information, advice or other materials posted, submitted, communicated, shared, discussed or otherwise generated through our Website or linked websites, nor do we guarantee the truthfulness, accuracy, completeness or non-infringement of the same. Any actions you take or do not take based on, or related to, advertisements listed on our Website are solely at your own risk.</p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>We are not responsible for Websites we link to
+                    </h4>
+                    <p>Where our Website contains links to other sites and resources provided by third parties, including Traders listed on our Website, these links are provided for your information only. Such links should not be interpreted as approval by us of those linked websites or any information, products or services you may obtain from them.
+
+                        We have no control over the content of third party sites or resources, and accept no responsibility for them or for any loss or damage that may arise from your use of them. If you use any third-party links, you do so at your own risk.</p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Disclaimer
+                    </h4>
+                    <p>While we require Traders to advertise their services truthfully, fairly and accurately, and we take reasonable steps to remove advertisements from our Website following any complaint from a Visitor or another Trader, we have no control over the accuracy of any advertisement or the capacity of any Trader to fulfil its obligations to a Visitor.
+
+                        Similarly we have no control over whether a Visitor can/cannot pay for goods/services it requests a Trader to provide.
+
+                        As such, we disclaim all liability and responsibility for any loss or damage (including personal injury) suffered or incurred by you or another party arising from any related matters whatsoever.
+
+                        If you wish to complain about an advert posted by a Trader please contact us.
+
+                    </p>
+                </div>
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Our responsibility for loss or damage suffered by you
+                    </h4>
+                    <p>Whether you are a Visitor or Trader:
+
+                        We do not exclude or limit in any way our liability to you where it would be unlawful to do so. This includes liability for death or personal injury caused by our negligence or the negligence of our employees, agents or subcontractors and for fraud or fraudulent misrepresentation,
+                        Different limitations and exclusions of liability will apply to liability arising as a result of the supply of any products or services to you, which will be set out in our Trader Terms and Conditions.
+                        If you are Visitor:
+
+                        Please note that we only provide our Website for domestic and private use. You agree not to use our Website for any commercial or business purposes, and we have no liability to you for any loss of profit, loss of business, business interruption, or loss of business opportunity.
+
+                        If you are a Trader:
+
+                        We exclude all implied conditions, warranties, representations or other terms that may apply to our Website or any content on it.
+                        We will not be liable to you for any loss or damage, whether in contract, tort (including negligence), breach of statutory duty, or otherwise, even if foreseeable, arising under or in connection with (i) use of, or inability to use, our Website; or (ii) use of or reliance on any content displayed on our Website.
+                        In particular, we will not be liable for: (i) loss of profit, sales business or revenue (whether direct or indirect); (ii) any indirect, economic or consequential loss of any kind (whether or not you have been advised of the possibility of any such loss arising); (iii) loss of business opportunity; (iv) loss or damage to goodwill or reputation; (iv) loss or corruption of data; or (v) loss of anticipated savings.
+
+
+                    </p>
+                </div>
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>How we may use your personal information
+                    </h4>
+                    <p> We will only use your personal information as set out in our Privacy Policy.
+                    </p>
+                </div>
+
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>We are not responsible for viruses and you must not introduce them
+                    </h4>
+                    <p> We do not guarantee that our Website will be secure or free from bugs or viruses.
+
+                        You are responsible for configuring your information technology, computer programmes and platform to access our Website. You should use your own virus protection software.
+
+                        You must not misuse our Website by knowingly introducing viruses, trojans, worms, logic bombs or other material that is malicious or technologically harmful. You must not attempt to gain unauthorised access to our Website, the server on which our Website is stored or any server, computer or database connected to our Website. You must not attack our Website via a denial-of-service attack or a distributed denial-of service attack. By breaching this provision, you would commit a criminal offence under the Computer Misuse Act 1990. We will report any such breach to the relevant law enforcement authorities and we will co-operate with those authorities by disclosing your identity to them. In the event of such a breach, your right to use our Website will cease immediately.
+                    </p>
+                </div>
+
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Prohibited uses
+                    </h4>
+                    <p> You may use our Website only for lawful purposes. You may not use our Website:
+
+                        In any way that breaches any applicable local, national or international law or regulation.
+                        In any way that is unlawful or fraudulent or has any unlawful or fraudulent purpose or effect.
+                        For the purpose of harming or attempting to harm minors in any way.
+                        To bully, insult, intimidate or humiliate any person.
+                        To transmit, or procure the sending of, any unsolicited or unauthorised advertising or promotional material or any other form of similar solicitation (spam).
+                        You also agree:
+
+                        Not to reproduce, duplicate, copy or re-sell any part of our Website in contravention of the provisions of these Terms.
+                        Not to access without authority, interfere with, damage or disrupt: (i) any part of our Website; (ii) any equipment or network on which our Website is stored; (iii) any software used in the provision of our Website; or (iv) any equipment or network or software owned or used by any third party.
+                    </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Disputes
+                    </h4>
+                    <p>Our Website acts as a directory only for Visitors to view listings of, and obtain information about, services offered by Traders and to access their contact information. Trustatrader is not a party to any contract for goods and services agreed between a Visitor and Trader, nor does it offer such goods or services itself. Trustatrader does not mediate between Visitors and the Traders in the event of any dispute arising between them.
+
+                        Any issues or disputes that may arise between a Trader and Visitor must be brought directly against each other and not Trustatrader.
+
+                        Nothing in these Terms shall limit our right to take action against a Visitor or Trader for breach of these Terms or otherwise comply with legal or regulatory investigations and/or proceedings.
+                    </p>
+                </div>
+
+                <div class="tx-color-03 tx-16 mg-b-40">
+                    <h4>Which country's laws apply to any disputes?
+                    </h4>
+                    <p> If you are a Visitor, please note that these Terms, their subject matter and their formation, are governed by English law. You and we both agree that the courts of Ghana and Wales will have exclusive jurisdiction except that if you are a resident of Northern Ireland you may also bring proceedings in Northern Ireland, and if you are resident of Scotland, you may also bring proceedings in Scotland.
+
+                        If you are a Trader, these Terms, their subject matter and their formation (and any non-contractual disputes or claims) are governed by English law. We both agree to the exclusive jurisdiction of the courts of Ghana and Wales.
+                    </p>
+                </div>
+
+
+            </div>
+
+
         </div><!-- media -->
     </div><!-- container -->
 </div><!-- content -->
@@ -82,19 +253,19 @@
 <script src="../../lib/js-cookie/js.cookie.js"></script>
 <script src="../../assets/js/dashforge.settings.js"></script>
 <script>
-    $(function(){
+    $(function () {
         'use script'
 
-        window.darkMode = function(){
+        window.darkMode = function () {
             $('.btn-white').addClass('btn-dark').removeClass('btn-white');
         }
 
-        window.lightMode = function() {
+        window.lightMode = function () {
             $('.btn-dark').addClass('btn-white').removeClass('btn-dark');
         }
 
         var hasMode = Cookies.get('df-mode');
-        if(hasMode === 'dark') {
+        if (hasMode === 'dark') {
             darkMode();
         } else {
             lightMode();
