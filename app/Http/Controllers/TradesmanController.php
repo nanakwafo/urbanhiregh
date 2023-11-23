d50936da9a419868b5fc11b078c81c186a04af18
nanakwafo <nanamensah1140@gmail.com>
2023-11-23 18:29:02 +0000

    User registration bugs

--- app/Http/Controllers/TradesmanController.php
+++ app/Http/Controllers/TradesmanController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use App\TradesmanProfile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
+
 class TradesmanController extends Controller
 {
     //
     public function index(Request $request){
 
-        $tradesmenprofiles =  DB::table('tradesman_profiles')->get();
-
+        $tradesmenprofiles  = DB::table('users')
+                    ->join('tradesman_profiles', 'users.id', '=', 'tradesman_profiles.user_id')
+                    ->select('users.*', 'tradesman_profiles.*')
+                    ->get();
+//dd($tradesmenprofiles);
         return view('tradesmen',[
-            'tradesmenprofiles' => $tradesmenprofiles,
+            'tradesmenprofiles' =>  $tradesmenprofiles,
 
         ]);
     }
--- resources/views/tradesmen.blade.php
+++ resources/views/tradesmen.blade.php
@@ -80,8 +80,8 @@
                                     <a href="">
                                         <div class="avatar avatar-lg"><img src="/uploads/{{$tradesmenprofile->picture}}" class="rounded-circle" alt=""></div>
                                     </a>
-                                    <h5><a href="">{{$tradesmenprofile->name}}</a></h5>
-                                    <p><h6>{{$tradesmenprofile->skills}}</h6></p>
+                                    <h5><a href="">{{$tradesmenprofile->first_name .' '.$tradesmenprofile->last_name}}</a></h5>
+                                    <h6>{{$tradesmenprofile->trade}}</h6>
                                     <p>{{$tradesmenprofile->location}}</p>
                                     <button class="btn btn-block btn-white">Call</button>
                                 </div>
