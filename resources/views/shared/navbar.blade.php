97a16167cc61d9d171aaed50d5a0290d51b9bba0
nanakwafo <nanamensah1140@gmail.com>
2023-11-26 08:01:25 +0000

    fix stye on home page

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -11,16 +11,14 @@
         <ul class="nav navbar-menu">
             <li class="nav-label pd-l-15 pd-lg-l-25 d-lg-none">Main Navigation</li>
             <li class="nav-item">
-                <a href="/how-it-works" class="nav-link"><i data-feather="fast-forward"></i> How it Works</a>
-
+                <a href="/how-it-works" class="nav-link"><i data-feather="pie-chart"></i> How it Works</a>
             </li>
             <li class="nav-item ">
-                <a href="/register" class="nav-link"><i data-feather="arrow-right"></i>Join Us</a>
-
+                <a href="/register" class="nav-link"><i data-feather="package"></i>Join Us</a>
             </li>
             <li class="nav-item ">
 
-                <a href="/equipment-tools" class="nav-link"><i data-feather="tool"></i>Rent An Equipment For Work</a>
+                <a href=" {{ url('equipment-tools') }}" class="nav-link"><i data-feather="tool"></i>Rent An Equipment For Work</a>
             </li>
             <li class="nav-item ">
                 <a href="{{ url('viewtradesmen') }}" class="nav-link"><i data-feather="search"></i>Find A Tradesman</a>
