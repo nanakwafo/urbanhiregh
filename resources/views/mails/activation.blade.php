90be1d3e537b68eda8b102f6b8ad5b0012a9b645
nanakwafo <nanamensah1140@gmail.com>
2024-02-06 20:31:48 +0000

    fix wording in the activation email

--- resources/views/mails/activation.blade.php
+++ resources/views/mails/activation.blade.php
@@ -1,13 +1,11 @@
-<h2 style="color:#6783b8">This is a message from Urbanhire </h2>
+<h2>This is a message from Urbanhire </h2>
 
 <p>Please click the link to activate your Urbanhire account<br>
     <a href="{{env('APP_URL')}}/activate/{{$email}}/{{$code}}"> Activate your account</a><br>
     <br>
     <br>
     -from the team at Urbanhire.<br>
-    E: info@Urbanhire.com<br>
+    E: hello@Urbanhireghana.com<br>
 
-
-    {{--    QrcodePay. NZBN: 9429048603539--}}
-    Urbanhire
+    UrbanhireGhana
 </p>
